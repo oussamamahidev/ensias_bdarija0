5b6960f083d72ae812dfe6f1153b3c5f11ab4018
oussamamahidev <mahioussama523@gmail.com>
2025-02-22 14:49:25 +0000

    add pageTags

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -3,7 +3,8 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import Tag from "@/database/tag.model";
 
 export async function getTopInterectedTags(params : GetTopInteractedTagsParams ){
 
@@ -15,6 +16,19 @@ export async function getTopInterectedTags(params : GetTopInteractedTagsParams )
         if(!user) throw new Error(`User not found`);
 
         return [{_id: "1", name: 'Tag1'}, {_id: "2", name: 'Tag2'}]
+    }catch(err){
+        console.log(err);
+        throw err;
+}
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function getAllTags(params : GetAllTagsParams ){
+
+    try{
+        await connectToDatabase();
+        const tags = await Tag.find({});
+        return {tags};
     }catch(err){
         console.log(err);
         throw err;
