44c95468119ff2fc352c43dccadcb7ac633764e9
oussamamahidev <mahioussama523@gmail.com>
2025-02-25 16:06:29 +0000

    adding tagsDetails

---
+++ app/(root)/tags/[id]/page.tsx
@@ -0,0 +1,59 @@
+import QuestionCard from '@/components/cards/QuestionCard'
+import NoResult from '@/components/shared/NoResult'
+import LocalSearch from '@/components/shared/search/LocalSearch'
+import { Iquestion } from '@/database/question.model'
+import { getQuestionByTagId } from '@/lib/actions/tag.actions'
+import { URLProps } from '@/types'
+import React from 'react'
+
+const Page = async ({params, searchParams}: URLProps) => {
+    const result = await getQuestionByTagId({
+        tagId: params.id,
+        searchQuery : searchParams.q
+    })
+    console.log(result);
+  return (
+
+    <>
+    <h1 className="h1-bold text-dark100_light900"> {result.tagTitle}</h1>
+    <div className="mt-11 w-full">
+      <LocalSearch
+        route="/collection"
+        iconPosition="left"
+        imgSrc="/assets/icons/search.svg"
+        placeholder="Search for tag questions"
+        otherClasses="flex-1"
+      />
+      
+    </div>
+    <div className="mt-10 flex w-full flex-col gap-6">
+      {result.questions?.length > 0 ? (
+        result.questions.map((question: any) => (
+          <QuestionCard
+                key={question._id}
+                _id={question._id}
+                title={question.title}
+                tags={question.tags}
+                author={question.author}
+                upvotes={question.upvotes}
+                views={question.views}
+                createdAt={question.createdAt} answers={[]}          />
+        ))
+      ) : (
+        <NoResult 
+          title="There’s no tag questions to show"
+          description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. Your query could be the next big thing others learn from. Get involved! 💡"
+          link="/ask-question"
+          linktitle="Ask a Question"
+        />
+      )}
+    </div>
+    <div className="mt-10">
+      
+    </div>
+  </>
+  )
+
+}
+
+export default Page
--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -1,10 +1,11 @@
-
 "use server"
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
-import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
-import Tag from "@/database/tag.model";
+import { GetAllTagsParams, GetQuestionsByTagIdParams, GetTopInteractedTagsParams } from "./shared.types";
+import Tag, { ITag } from "@/database/tag.model";
+import { FilterQuery } from "mongoose";
+import Question from "@/database/question.model";
 
 export async function getTopInterectedTags(params : GetTopInteractedTagsParams ){
 
@@ -29,6 +30,41 @@ export async function getAllTags(params : GetAllTagsParams ){
         await connectToDatabase();
         const tags = await Tag.find({});
         return {tags};
+    }catch(err){
+        console.log(err);
+        throw err;
+}
+}
+
+
+export async function getQuestionByTagId(params : GetQuestionsByTagIdParams ){
+
+    try{
+        await connectToDatabase();
+        const {tagId, searchQuery} = params
+        const tagFilter: FilterQuery<ITag> = { _id: tagId };
+        console.log(tagFilter);
+        const tag = await Tag.findOne(tagFilter).populate({
+            path: "questions",
+            model: Question,
+            match: searchQuery
+              ? { title: { $regex: searchQuery, $options: "i" } }
+              : {},
+            options: {
+              sort: { createdAt: -1 },
+            },
+            populate: [
+              { path: "tags", model: Tag, select: "_id name" },
+              { path: "author", model: User, select: "_id clerkId name picture" },
+            ],
+          });
+          if (!tag) {
+            throw new Error("Tag not found");
+          }
+        console.log(tag);
+      const questions= tag?.questions;
+      console.log("tag fiha had ",questions);
+      return {tagTitle: tag?.name, questions};
     }catch(err){
         console.log(err);
         throw err;
