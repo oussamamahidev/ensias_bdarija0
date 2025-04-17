55acbf065f4de0f769535579af58e40c3facefc4
oussamamahidev <mahioussama523@gmail.com>
2025-04-17 15:57:46 +0000

    adding analytics

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,9 @@
-import React from "react"
-
-import { Toaster } from "@/components/ui/toaster"
-import Navbar from "@/components/shared/navbar/Navbar"
-import LeftSidebar from "@/components/shared/LeftSidbar"
-import RightSidebar from "@/components/shared/RightSiedbar"
+import React from "react";
+import { Analytics } from "@vercel/analytics/react";
+import { Toaster } from "@/components/ui/toaster";
+import Navbar from "@/components/shared/navbar/Navbar";
+import LeftSidebar from "@/components/shared/LeftSidbar";
+import RightSidebar from "@/components/shared/RightSiedbar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -13,15 +13,17 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <LeftSidebar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14 ml-[266px] xl:mr-[350px] transition-all duration-200">
           <div className="mx-auto w-full max-w-5xl">
-            <React.Suspense fallback={<div>Loading...</div>}>{children}</React.Suspense>
+            <React.Suspense fallback={<div>Loading...</div>}>
+              {children}
+              <Analytics />
+            </React.Suspense>
           </div>
         </section>
         <RightSidebar />
       </div>
       <Toaster />
     </main>
-  )
-}
+  );
+};
 
-export default Layout
-
+export default Layout;
